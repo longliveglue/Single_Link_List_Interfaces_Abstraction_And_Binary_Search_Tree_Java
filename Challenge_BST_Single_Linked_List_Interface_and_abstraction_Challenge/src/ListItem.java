c99489ff5b8b16c7af41e6d9279678529a1ac6f4
thomashamilton <thomas.hamil2011@gmail.com>
2026-01-10 17:34:04 +0000

    Second commit, Abstract Class list item is now complete. Next step is to create the Node class.

--- Challenge_BST_Single_Linked_List_Interface_and_abstraction_Challenge/src/ListItem.java
+++ Challenge_BST_Single_Linked_List_Interface_and_abstraction_Challenge/src/ListItem.java
@@ -1,5 +1,5 @@
 //This is the abstract class that will create the Nodes.
-//In this program we have two different data structures and linked list and then a Binary Search Tree.
+//In this program we have two different data structures a linked list and then a Binary Search Tree.
 //This ListItem is simply forcing me to demonstrate an abstract class so that i can then extend it into the Node Class.
 
 
@@ -14,6 +14,40 @@ public abstract class ListItem {
 
     public ListItem next(){
     return rightLink;
+    }
+
+    public ListItem setNext(ListItem nextItem){
+        rightLink = nextItem;
+        return rightLink;
+    }
+
+    public ListItem previous(){
+        return leftLink;
+    }
+
+    public ListItem setPrevious(ListItem prevItem){
+        leftLink = prevItem;
+        return leftLink;
+    }
+
+    public int compareTo(ListItem itemToCompare){
+        int currentItem = (int) value;
+        int newItem = (int) itemToCompare.getValue();
+
+        if (currentItem == newItem){
+            return 0;
+        } else if ( currentItem > newItem) {
+            return 1;
+        }
+        return -1;
+    }
+
+    public Object getValue(){
+        return value;
+    }
+
+    public void setValue(Object setValue){
+        value = setValue;
     }
 
 }
